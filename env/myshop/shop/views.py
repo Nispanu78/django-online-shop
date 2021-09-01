cfa278a88e06646ffd420f5816b039a380950004
Nicola <nicspanu@gmail.com>
2021-09-01 15:01:29 +0200

    views

--- env/myshop/shop/views.py
+++ env/myshop/shop/views.py
@@ -14,3 +14,12 @@ def product_list(request, category_slug = None):
                 {'category': category,
                  'categories': categories,
                  'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                id = id,
+                slug = slug,
+                available = True)
+    return render(request,
+                'shop/product_detail.html',
+                {'product': product})
