57658a5ffa81bc13364f172b4e7383c4c7ac6671
Nicola <nicspanu@gmail.com>
2021-09-13 11:17:22 +0200

    celery

--- env/myshop/settings.py
+++ env/myshop/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# E-mail
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
